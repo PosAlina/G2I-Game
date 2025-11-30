928aea4f84cb12fc768c4496a2820769ee08f24f
slylii <102788866+slylii@users.noreply.github.com>
2025-11-30 23:26:10 +0300

    feat: minimal HUD

---
+++ Source/G2I/Private/UI/G2IHUD.cpp
@@ -0,0 +1,25 @@
+#include "UI/G2IHUD.h"
+
+#include "G2IPlayerController.h"
+#include "Blueprint/UserWidget.h"
+
+inline void AG2IHUD::BeginPlay()
+{
+	Super::BeginPlay();
+	InitHud();
+	AddWidgetToViewport(InputDescriptionWidget);
+}
+
+void AG2IHUD::InitHud()
+{
+	APlayerController* pc = Cast<AG2IPlayerController>(GetOwner());
+	InputDescriptionWidget = CreateWidget<UUserWidget>(pc, InputDescriptionWidgetClass);
+}
+
+void AG2IHUD::AddWidgetToViewport(UUserWidget* WidgetToAdd)
+{
+	if (WidgetToAdd)
+	{
+		WidgetToAdd->AddToViewport();
+	}
+}
