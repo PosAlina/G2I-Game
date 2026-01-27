ff5653b56c9c0e7ab0262f4b52a8a736c79d5841
Keendaj <167191489+Keendaj@users.noreply.github.com>
2026-01-27 15:52:36 +0300

    Save befor auto compile

---
+++ Source/G2I/Private/Gameplay/G2IMovingBySteamAndHandsObject.cpp
@@ -0,0 +1,27 @@
+
+
+
+#include "Gameplay/G2IMovingBySteamAndHandsObject.h"
+
+// Sets default values
+AG2IMovingBySteamAndHandsObject::AG2IMovingBySteamAndHandsObject()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AG2IMovingBySteamAndHandsObject::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AG2IMovingBySteamAndHandsObject::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
