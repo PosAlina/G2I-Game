03e88ecbfd9cd7daab4a9b0e320c495802ff29a5
Karaseva Irina <94855816+irazaurus@users.noreply.github.com>
2026-01-31 14:30:48 +0300

    feat: save game base
    * added Savable tag + interface in GameInstance
    * added save & load functions
    * added player's locations in the SaveGame for testing

---
+++ Source/G2I/Private/Interfaces/SavingSystem/G2ISavableInterface.cpp
@@ -0,0 +1,6 @@
+
+
+
+#include "Interfaces/SavingSystem/G2ISavableInterface.h"
+
+// Add default functionality here for any IG2ISavableInterface functions that are not pure virtual.
---
+++ Source/G2I/Public/Game/G2IGameplaySaveGame.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/SaveGame.h"
+#include "G2IGameplaySaveGame.generated.h"
+
+USTRUCT(BlueprintType)
+struct FPlayersSaveData
+{
+	GENERATED_BODY()
+
+	UPROPERTY(SaveGame, BlueprintReadWrite)
+	TMap<FString, FVector> CharactersLocation;
+
+	UPROPERTY(SaveGame, BlueprintReadWrite)
+	FString CurrentCharacter;
+};
+
+/**
+ * SaveGame for gameplay data.
+ */
+UCLASS()
+class G2I_API UG2IGameplaySaveGame : public USaveGame
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(SaveGame, BlueprintReadWrite, Category = "Save Gameplay Data|Player")
+	FPlayersSaveData PlayersSaveData;
+};
