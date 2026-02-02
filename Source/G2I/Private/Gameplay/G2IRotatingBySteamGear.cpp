2aadb30f74d0b77946fa247d2025b122bb43b934
Keendaj <167191489+Keendaj@users.noreply.github.com>
2026-02-02 15:02:41 +0300

    Done

---
+++ Source/G2I/Private/Gameplay/G2IRotatingBySteamGear.cpp
@@ -0,0 +1,80 @@
+#include "Gameplay/G2IRotatingBySteamGear.h"
+#include "G2I.h"
+#include "Interfaces/G2IMovingByGearObjectInterface.h"
+
+AG2IRotatingBySteamGear::AG2IRotatingBySteamGear()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	Timeline = CreateDefaultSubobject<UTimelineComponent>(TEXT("SteamForceTimeline"));
+	TimelineValue = 0.0f;
+	TimelineCurve = nullptr;
+}
+
+void AG2IRotatingBySteamGear::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (!TimelineCurve) {
+		UE_LOG(LogG2I, Warning, TEXT("TimelineCurve is not set for %s"), *GetName());
+		return;
+	}
+
+	TimelineUpdate.BindUFunction(this, FName("OnTimelineUpdate"));
+
+	Timeline->AddInterpFloat(TimelineCurve, TimelineUpdate);
+	Timeline->SetTimelineLengthMode(ETimelineLengthMode::TL_LastKeyFrame);
+	Timeline->SetLooping(false);
+}
+
+void AG2IRotatingBySteamGear::OnShoot_Implementation(const FHitResult& HitResult, AActor* Character) {
+	//Wrap into pushing with use of all axis
+	//Calculating direction based on trace and rotation normal
+	FVector HitVector = HitResult.ImpactPoint - HitResult.TraceStart;
+	HitVector.X = HitVector.Z;
+	HitVector.Z = 0;
+	FVector RotationNormal = { 1.0f, 0.0f, 0.0f };
+	float CosineValue = FVector::DotProduct(HitVector.GetSafeNormal(), RotationNormal.GetSafeNormal());
+
+	//Calculating direction based on HitNormal and axis Z
+	FVector HitNormal = HitResult.ImpactNormal;
+	HitNormal.X = 0;
+	FVector AxisZ = { 0.0f, 0.0f, 1.0f };
+	FVector CrossProductResult = FVector::CrossProduct(AxisZ, HitNormal);
+
+	RotationSign = CrossProductResult.X * CosineValue;
+
+	if (!Timeline) {
+		UE_LOG(LogG2I, Error, TEXT("Can't start timeline for %s"), *GetName());
+		return;
+	}
+
+	Timeline->Stop();
+	Timeline->PlayFromStart();
+}
+
+void AG2IRotatingBySteamGear::OnTimelineUpdate(float Output)
+{
+	for (const auto& i : MovableObjects) {
+		if (!i) {
+			UE_LOG(LogG2I, Error, TEXT("Can't get object to push for %s"), *GetName());
+			continue;
+		}
+
+		if (i->Implements<UG2IMovingByGearObjectInterface>()) {
+			IG2IMovingByGearObjectInterface::Execute_OnPushing(i, Output * RotationSpeed * RotationSign);
+		}
+	}
+
+	FRotator ActorRotator = { 0.0f, 0.0f, 0.0f };
+	if (bRotateRoll) {
+		ActorRotator.Roll = Output * RotationSpeed * RotationSign;
+	}
+	if (bRotatePitch) {
+		ActorRotator.Pitch = Output * RotationSpeed * RotationSign;
+	}
+	if (bRotateYaw) {
+		ActorRotator.Yaw = Output * RotationSpeed * RotationSign;
+	}
+	AddActorWorldRotation(ActorRotator);
+}
--- Source/G2I/Public/Gameplay/G2IMovingBySteamAndHandsObject.h
+++ Source/G2I/Public/Gameplay/G2IMovingBySteamAndHandsObject.h
@@ -1,29 +1,52 @@
 
 
 #pragma once
-
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
-#include "Interfaces/G2ITraceableObectInterface.h"
 #include "Interfaces/G2IInteractiveObjectInterface.h"
+#include "Interfaces/G2ITraceableObectInterface.h"
+#include "Interfaces/G2IMovingObjectInterface.h"
+#include "Components/TimelineComponent.h"
 #include "G2IMovingBySteamAndHandsObject.generated.h"
 
+class UPhysicsConstraintComponent;
 UCLASS()
-class G2I_API AG2IMovingBySteamAndHandsObject : public AActor, public IG2ITraceableObectInterface, public IG2IInteractiveObjectInterface
+class G2I_API AG2IMovingBySteamAndHandsObject : public AActor, public IG2ITraceableObectInterface, public IG2IInteractiveObjectInterface, public IG2IMovingObjectInterface
 {
 	GENERATED_BODY()
-	
+
+private:
+	FOnTimelineFloat TimelineUpdate;
+
+	FVector ForwardVector;
+
+	float Multiplier;
+
+	TObjectPtr<UTimelineComponent> Timeline;
+
+	float TimelineValue;
 protected:	
 	AG2IMovingBySteamAndHandsObject();
 
-public:
-
-	bool CanInteract_Implementation(const ACharacter* Interactor) override;
-
-	void Interact_Implementation(const ACharacter* Interactor) override;
-
-	void OnShoot_Implementation(const FHitResult& HitResult, AActor* Character) override;
+	UPROPERTY(EditAnywhere, Category = "Data|SteamTimelineData")
+	TObjectPtr<UCurveFloat> TimelineCurve;
 
 	UPROPERTY(EditAnywhere, Category = "Data")
-	TSet<ACharacter*> PossibleInteractors;
+	TSet<TObjectPtr<ACharacter>> PossibleInteractors;
+
+	UPROPERTY(EditAnywhere, Category = "Data")
+	float BounceMultiplier;
+
+	UPROPERTY(EditAnywhere, Category = "Data")
+	float SteamPushForce;
+
+	UFUNCTION(BlueprintCallable, Category = "Timeline")
+	void OnTimelineUpdate(float Output);
+
+	void BeginPlay() override;
+public:
+	bool CanInteract_Implementation(const ACharacter* Interactor) override;
+	void Interact_Implementation(ACharacter* Interactor) override;
+	void OnShoot_Implementation(const FHitResult& HitResult, AActor* Character) override;
+	float GetSpeedChange_Implementation() override;
 };
