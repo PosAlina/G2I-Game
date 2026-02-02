09a405435346d9ca32fa16bbdabca427524ee1f7
Keendaj <167191489+Keendaj@users.noreply.github.com>
2026-02-02 15:02:41 +0300

    Done

--- Source/G2I/Public/Interfaces/G2IMovingByGearObjectInterface.h
+++ Source/G2I/Public/Interfaces/G2IMovingByGearObjectInterface.h
@@ -6,16 +6,12 @@
 #include "UObject/Interface.h"
 #include "G2IMovingByGearObjectInterface.generated.h"
 
-// This class does not need to be modified.
-UINTERFACE(MinimalAPI)
+UINTERFACE(MinimalAPI, Blueprintable)
 class UG2IMovingByGearObjectInterface : public UInterface
 {
 	GENERATED_BODY()
 };
 
-/**
- * 
- */
 class G2I_API IG2IMovingByGearObjectInterface
 {
 	GENERATED_BODY()
