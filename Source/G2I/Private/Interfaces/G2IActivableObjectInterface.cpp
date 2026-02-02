e60bffb2abac051ba9587a1d1a4baf2a39baca5b
Keendaj <167191489+Keendaj@users.noreply.github.com>
2026-02-02 18:48:35 +0300

    Fix again

--- Source/G2I/Private/Interfaces/G2IActivableObjectInterface.cpp
+++
@@ -1,6 +0,0 @@
-
-
-
-#include "Interfaces/G2IActivableObjectInterface.h"
-
-// Add default functionality here for any IG2IActivableObjectInterface functions that are not pure virtual.
--- Source/G2I/Private/Interfaces/G2ICatchableObjectInterface.cpp
+++
@@ -1,6 +0,0 @@
-
-
-
-#include "Interfaces/G2ICatchableObjectInterface.h"
-
-// Add default functionality here for any IG2ICatchableObjectInterface functions that are not pure virtual.
