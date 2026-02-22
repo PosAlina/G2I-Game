50595fbc13e687b3b8456374aac30890fe30f5bf
Karaseva Irina <94855816+irazaurus@users.noreply.github.com>
2026-02-22 18:40:09 +0300

    feat: activation with order

---
+++ Source/G2I/Private/Components/G2IActivationWithOrderComponent.cpp
@@ -0,0 +1,44 @@
+
+#include "Components/G2IActivationWithOrderComponent.h"
+
+UG2IActivationWithOrderComponent::UG2IActivationWithOrderComponent()
+{
+}
+
+void UG2IActivationWithOrderComponent::Activated()
+{
+	if (bActivated)
+	{
+		if (bCanBeReactivated)
+		{
+			bActivated = false;
+			OnActivatedDelegate.ExecuteIfBound(GetOwner(), true, this);
+		}
+	}
+	else
+	{
+		bActivated = true;
+		OnActivatedDelegate.ExecuteIfBound(GetOwner(), false, this);
+	}
+}
+
+void UG2IActivationWithOrderComponent::Accepted(int32 AtIndex)
+{
+	ActivatedIndexes.Push(AtIndex);
+}
+
+void UG2IActivationWithOrderComponent::Declined()
+{
+	ActivatedIndexes.Pop();
+}
+
+void UG2IActivationWithOrderComponent::Restored()
+{
+	ActivatedIndexes.Empty();
+	bActivated = false;
+}
+
+void UG2IActivationWithOrderComponent::BeginPlay()
+{
+	Super::BeginPlay();
+}
