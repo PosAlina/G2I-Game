05dfe8e476ea0dde0023e43b452a5848c6082186
slylii <102788866+slylii@users.noreply.github.com>
2026-03-01 22:18:29 +0300

    fix: checks and small fixes

--- Source/G2I/Private/Gameplay/G2IColorZoneComponent.cpp
+++ Source/G2I/Private/Gameplay/G2IColorZoneComponent.cpp
@@ -1,12 +1,18 @@
-
-
-
 #include "Gameplay/G2IColorZoneComponent.h"
+
+#include "G2I.h"
 #include "Components/BoxComponent.h"
 
 UG2IColorZoneComponent::UG2IColorZoneComponent()
 {
 	ZoneCol = CreateDefaultSubobject<UBoxComponent>(TEXT("ZoneCol"));
+	
+	if (!ensure(ZoneCol))
+	{
+		UE_LOG(LogG2I, Error, TEXT("ZoneColor was not created in %s"), *GetName());
+		return;
+	}
+	
 	ZoneCol->SetupAttachment(this);
 }
 
