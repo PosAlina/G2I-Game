c06d76eb1713e098807f31437342e0e40707a92d
Keendaj <167191489+Keendaj@users.noreply.github.com>
2026-03-26 23:16:25 +0300

    Added Furnace Door, code style changes and MovingWithSplineActor refact

---
+++ Source/G2I/Private/Gameplay/G2IFurnaceDoor.cpp
@@ -0,0 +1,57 @@
+#include "Gameplay/G2IFurnaceDoor.h"
+#include "TimerManager.h"
+#include "G2I.h"
+
+void AG2IFurnaceDoor::StartTick()
+{
+	CurrentTicks = 0;
+	
+	if (MaxTicks <= 0) {
+		return;
+	}
+
+	UWorld* World = GetWorld();
+	if (!ensure(World)) {
+		UE_LOG(LogG2I, Error, TEXT("World doesn't exist in %s"), *GetName());
+		return;
+	}
+
+	float Interval = TotalTime / MaxTicks;
+
+	World->GetTimerManager().SetTimer(
+		PushTimerHandle,
+		this,
+		&AG2IFurnaceDoor::PerformTick,
+		Interval,
+		true
+	);
+}
+
+void AG2IFurnaceDoor::PerformTick()
+{
+	OnPushing_Implementation(Force);
+
+	CurrentTicks++;
+
+	if (CurrentTicks < MaxTicks)
+	{
+		return;
+	}
+
+	UWorld* World = GetWorld();
+	if (!ensure(World)) {
+		UE_LOG(LogG2I, Error, TEXT("World doesn't exist in %s"), *GetName());
+		return;
+	}
+
+	World->GetTimerManager().ClearTimer(PushTimerHandle);
+}
+
+void AG2IFurnaceDoor::Activate_Implementation()
+{
+	StartTick();
+}
+
+void AG2IFurnaceDoor::Deactivate_Implementation() {
+	return;
+}
