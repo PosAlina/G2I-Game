e6ad26d99405aac29d53d6a58dd920842faaeaff
Keendaj <167191489+Keendaj@users.noreply.github.com>
2026-03-28 23:38:14 +0300

    Feat: button which activates other actors

---
+++ Source/G2I/Public/Gameplay/G2IButtonActivator.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Interfaces/G2IInteractiveObjectInterface.h"
+#include "G2IButtonActivator.generated.h"
+
+class UG2ILauncherComponent;
+
+UCLASS()
+class G2I_API AG2IButtonActivator : public AActor, public IG2IInteractiveObjectInterface
+{
+	GENERATED_BODY()
+
+protected:
+
+	UPROPERTY(EditAnywhere, Category = "Data|Activation")
+	TSet<TObjectPtr<AActor>> ActorsToActivate;
+
+	virtual void BeginPlay() override;
+
+public:	
+	AG2IButtonActivator();
+
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<UG2IWorldHintKeyWidgetComponent> HintKeyWidgetComp;
+
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<UG2ILauncherComponent> LauncherComp;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	TObjectPtr<UStaticMeshComponent> StaticMeshComponent;
+
+	virtual bool CanInteract_Implementation(const ACharacter* Interactor) override;
+	virtual void Interact_Implementation(const ACharacter* Interactor) override;
+	virtual UG2IWorldHintKeyWidgetComponent* GetInteractionKeyHintComponent_Implementation() override;
+};
