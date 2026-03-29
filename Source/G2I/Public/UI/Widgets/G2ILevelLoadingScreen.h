b1f317e3e3680b6c71dcc0f9c8e0cb3aa791e4c9
Alina Pospelova <pospelowa.alina@yandex.ru>
2026-03-29 16:17:12 +0300

    feat: Loading Screen

---
+++ Source/G2I/Public/UI/Widgets/G2ILevelLoadingScreen.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "G2IUserWidget.h"
+#include "G2ILevelLoadingScreen.generated.h"
+
+UCLASS()
+class G2I_API UG2ILevelLoadingScreen : public UG2IUserWidget
+{
+	GENERATED_BODY()
+};
