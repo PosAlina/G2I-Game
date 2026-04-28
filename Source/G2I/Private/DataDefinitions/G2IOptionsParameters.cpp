90b4036bb8d3fa2b867044f50e806beb79020ba2
Alina Pospelova <pospelowa.alina@yandex.ru>
2026-04-28 16:55:45 +0300

    feat: Add Defraults of Mouse Sensitive in Options

---
+++ Source/G2I/Private/DataDefinitions/G2IOptionsParameters.cpp
@@ -0,0 +1,8 @@
+#include "DataDefinitions/G2IOptionsParameters.h"
+
+void UG2IOptionsParameters::SetupDefaultParameters()
+{
+	MouseSensitive = DefaultMouseSensitive;
+	bIsInvertedCameraHorizontalRotation = bIsDefaultInvertedCameraHorizontalRotation;
+	bIsInvertedCameraVerticalRotation = bIsDefaultInvertedCameraVerticalRotation;
+}
