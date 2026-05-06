a4ffc0954e65db8b4c2d15498e17aa9808fadf57
Keendaj <167191489+Keendaj@users.noreply.github.com>
2026-05-06 18:33:07 +0300

    feat: add hype animatioons
    JOOOOOOOOU
    
    feat: added final zvuks to angar
    
    1) Changed listener position
    2) Added launcher to audio zone
    3) Расставил zvuks
    4) Fixed Delay
    
    Co-Authored-By: Amigo509 <189549087+Amigo509@users.noreply.github.com>

--- Source/G2I/Private/Components/G2IAudioZoneComponent.cpp
+++ Source/G2I/Private/Components/G2IAudioZoneComponent.cpp
@@ -2,30 +2,37 @@
 #include "G2I.h"
 #include "Components/PrimitiveComponent.h"
 #include "G2ISoundComponent.h"
+#include "LaunchingIndication/G2ILauncherComponent.h"
 
 UG2IAudioZoneComponent::UG2IAudioZoneComponent()
 {
 	SoundComp = CreateDefaultSubobject<UG2ISoundComponent>(TEXT("SoundComponent"));
 
 	if (!ensure(SoundComp)) {
-		UE_LOG(LogG2I, Error, TEXT("Couldn't create SoundComponent in %s"), *GetName());
+		UE_LOG(LogG2I, Error, TEXT("Couldn't create SoundComponent in %s"), *GetNameSafe(GetOwner()));
 		return;
 	}
 	else {
 		SoundComp->SetupSounds.Add(PlayingSoundName, FSoundConfig());
+	}
+
+	LauncherComp = CreateDefaultSubobject<UG2ILauncherComponent>(TEXT("LauncherComp"));
+	if (!ensure(LauncherComp))
+	{
+		UE_LOG(LogG2I, Error, TEXT("%s: Couldn't create %s"), *GetNameSafe(GetOwner()),
+			*UG2ILauncherComponent::StaticClass()->GetName());
 	}
 }
 
 void UG2IAudioZoneComponent::BeginPlay()
 {
 	Super::BeginPlay();
-	if (AActor* Owner = GetOwner()) {
 
+	if (AActor* Owner = GetOwner()) {
 		AudioZoneTriggerCollision = Cast<UPrimitiveComponent>(AudioZoneTriggerCollisionRef.GetComponent(Owner));
 		if (!ensure(AudioZoneTriggerCollision))
 		{
-
-			UE_LOG(LogG2I, Error, TEXT("UG2IAudioZoneComponent on %s: TriggerCollision isn't set"), *Owner->GetName());
+			UE_LOG(LogG2I, Error, TEXT("UG2IAudioZoneComponent on %s: TriggerCollision isn't set"), *GetNameSafe(Owner));
 		}
 		else
 		{
@@ -34,15 +41,15 @@ void UG2IAudioZoneComponent::BeginPlay()
 		}
 	}
 	else {
-		UE_LOG(LogG2I, Error, TEXT("UG2IAudioZoneComponent: can't get Owner"));
+		UE_LOG(LogG2I, Error, TEXT("UG2IAudioZoneComponent: can't get Owner. Component name: %s"), *GetNameSafe(Owner));
 	}
 
 	if (!ensure(SoundComp))
 	{
-		UE_LOG(LogG2I, Error, TEXT("SoundComponent doesn't exist in %s"), *GetName());
+		UE_LOG(LogG2I, Error, TEXT("SoundComponent doesn't exist in %s"), *GetNameSafe(GetOwner()));
 		return;
 	}
-	
+
 	if (const auto* SoundConf = SoundComp->SetupSounds.Find(PlayingSoundName)) {
 		PlayingSoundId = SoundComp->AddSound(*SoundConf);
 	}
@@ -50,13 +57,19 @@ void UG2IAudioZoneComponent::BeginPlay()
 
 void UG2IAudioZoneComponent::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
+	if (!ensure(LauncherComp)) {
+		UE_LOG(LogG2I, Error, TEXT("UG2IAudioZoneComponent on %s: can't get Launcher component"), *GetNameSafe(GetOwner()));
+		return;
+	}
+
+	if (LauncherComp->IsLocked_Implementation()) {
+		UE_LOG(LogG2I, Verbose, TEXT("UG2IAudioZoneComponent on %s: Launcher is locked"), *GetNameSafe(GetOwner()));
+		return;
+	}
+
 	if (!ensure(OtherActor)) {
-		if (const AActor* Owner = GetOwner()) {
-			UE_LOG(LogG2I, Error, TEXT("UG2IAudioZoneComponent on %s: can't get Other Actor"), *Owner->GetName());
-		}
-		else {
-			UE_LOG(LogG2I, Error, TEXT("UG2IAudioZoneComponent: can't get Other Actor and can't get Owner"));
-		}
+		UE_LOG(LogG2I, Error, TEXT("UG2IAudioZoneComponent on %s: can't get Other Actor"), *GetNameSafe(GetOwner()));
+		return;
 	}
 
 	if (!AllowedActors.Contains(OtherActor))
@@ -66,7 +79,7 @@ void UG2IAudioZoneComponent::OnOverlapBegin(UPrimitiveComponent* OverlappedCompo
 
 	if (!ensure(SoundComp))
 	{
-		UE_LOG(LogG2I, Error, TEXT("SoundComponent doesn't exist in %s"), *GetName());
+		UE_LOG(LogG2I, Error, TEXT("SoundComponent doesn't exist in %s"), *GetNameSafe(GetOwner()));
 		return;
 	}
 
@@ -80,12 +93,8 @@ void UG2IAudioZoneComponent::OnOverlapEnd(UPrimitiveComponent* OverlappedCompone
 	}
 
 	if (!ensure(OtherActor)) {
-		if (const AActor* Owner = GetOwner()) {
-			UE_LOG(LogG2I, Error, TEXT("UG2IAudioZoneComponent on %s: can't get Other Actor"), *Owner->GetName());
-		}
-		else {
-			UE_LOG(LogG2I, Error, TEXT("UG2IAudioZoneComponent: can't get Other Actor and can't get Owner"));
-		}
+		UE_LOG(LogG2I, Error, TEXT("UG2IAudioZoneComponent on %s: can't get Other Actor"), *GetNameSafe(GetOwner()));
+		return;
 	}
 
 	if (!AllowedActors.Contains(OtherActor))
@@ -95,7 +104,7 @@ void UG2IAudioZoneComponent::OnOverlapEnd(UPrimitiveComponent* OverlappedCompone
 
 	if (!ensure(SoundComp))
 	{
-		UE_LOG(LogG2I, Error, TEXT("SoundComponent doesn't exist in %s"), *GetName());
+		UE_LOG(LogG2I, Error, TEXT("SoundComponent doesn't exist in %s"), *GetNameSafe(GetOwner()));
 		return;
 	}
 
