012c70f483e09d0af2a1cb545ce92e74ecc970c4
Alina Pospelova <pospelowa.alina@yandex.ru>
2026-05-13 01:03:01 +0300

    feat: UI Screen Options

--- Source/G2I/Private/DataDefinitions/G2IOptionsParameters.cpp
+++ Source/G2I/Private/DataDefinitions/G2IOptionsParameters.cpp
@@ -1,8 +1,126 @@
 #include "DataDefinitions/G2IOptionsParameters.h"
+#include "G2I.h"
+#include "GameFramework/GameUserSettings.h"
 
 void UG2IOptionsParameters::SetupDefaultParameters()
 {
+	Settings = UGameUserSettings::GetGameUserSettings();
+	if (!ensure(Settings))
+	{
+		UE_LOG(LogG2I, Error, TEXT("%s: Couldn't find Game User Settings"), *GetName());
+	}
+	
 	MouseSensitive = DefaultMouseSensitive;
 	bIsInvertedCameraHorizontalRotation = bIsDefaultInvertedCameraHorizontalRotation;
 	bIsInvertedCameraVerticalRotation = bIsDefaultInvertedCameraVerticalRotation;
+	
+	SetScreenMode(DefaultScreenMode);
+	SetScreenResolution(DefaultScreenResolution);
+	SetFrameRate(DefaultFrameRate);
+	SetVSync(bIsDefaultVSyncOn);
+	//SetBrightness(DefaultBrightness);
+}
+
+void UG2IOptionsParameters::SetScreenMode(const EG2IScreenMode NewScreenMode)
+{
+	ScreenMode = NewScreenMode;
+	
+	if (!ensure(Settings))
+	{
+		UE_LOG(LogG2I, Error, TEXT("%s: Couldn't find Game User Settings"), *GetName());
+		return;
+	}
+	switch (ScreenMode)
+	{
+	case EG2IScreenMode::Windowed:
+		Settings->SetFullscreenMode(EWindowMode::Windowed);
+		break;
+	case EG2IScreenMode::WindowedFullscreen:
+		Settings->SetFullscreenMode(EWindowMode::WindowedFullscreen);
+		break;
+	case EG2IScreenMode::Fullscreen:
+		Settings->SetFullscreenMode(EWindowMode::Fullscreen);
+		break;
+	default: ;
+	}
+	Settings->ApplySettings(true);
+}
+
+void UG2IOptionsParameters::SetScreenResolution(const EG2IScreenResolution NewScreenResolution)
+{
+	ScreenResolution = NewScreenResolution;
+
+	if (!ensure(Settings))
+	{
+		UE_LOG(LogG2I, Error, TEXT("%s: Couldn't find Game User Settings"), *GetName());
+		return;
+	}
+	switch (ScreenResolution)
+	{
+	case EG2IScreenResolution::Resolution1280On720:
+		Settings->SetScreenResolution(FIntPoint(1280, 720));
+		break;
+	case EG2IScreenResolution::Resolution1920On1080:
+		Settings->SetScreenResolution(FIntPoint(1920, 1080));
+		break;
+	case EG2IScreenResolution::Resolution2560On1440:
+		Settings->SetScreenResolution(FIntPoint(2560, 1440));
+		break;
+	case EG2IScreenResolution::Resolution3840On2160:
+		Settings->SetScreenResolution(FIntPoint(3840, 2160));
+		break;
+	default: ;
+	}
+	Settings->ApplySettings(true);
+}
+
+void UG2IOptionsParameters::SetFrameRate(const EG2IFrameRate NewFrameRate)
+{
+	FrameRate = NewFrameRate;
+	
+	if (!ensure(Settings))
+	{
+		UE_LOG(LogG2I, Error, TEXT("%s: Couldn't find Game User Settings"), *GetName());
+		return;
+	}
+	switch (FrameRate)
+	{
+	case EG2IFrameRate::FPS30:
+		Settings->SetFrameRateLimit(30.f);
+		break;
+	case EG2IFrameRate::FPS60:
+		Settings->SetFrameRateLimit(60.f);
+		break;
+	case EG2IFrameRate::FPS120:
+		Settings->SetFrameRateLimit(120.f);
+		break;
+	case EG2IFrameRate::FPSUnlimited:
+		Settings->SetFrameRateLimit(0.f);
+		break;
+	default: ;
+	}
+	Settings->ApplySettings(true);
+}
+
+/*void UG2IOptionsParameters::SetBrightness(const float NewBrightness)
+{
+	Brightness = NewBrightness;
+	if (APostProcessVolume *PostProcessVolume =
+	 *Cast<APostProcessVolume>(UGameplayStatics::GetActorOfClass(GetWorld(), APostProcessVolume::StaticClass())))
+	{
+		PostProcessVolume->Settings.AutoExposureBias = Brightness;
+	}
+}*/
+
+void UG2IOptionsParameters::SetVSync(const bool bIsNewVSync)
+{
+	bIsVSyncOn = bIsNewVSync;
+	
+	if (!ensure(Settings))
+	{
+		UE_LOG(LogG2I, Error, TEXT("%s: Couldn't find Game User Settings"), *GetName());
+		return;
+	}
+	Settings->SetVSyncEnabled(bIsVSyncOn);
+	Settings->ApplySettings(true);
 }
--- Source/G2I/Public/DataDefinitions/G2IOptionsParameters.h
+++ Source/G2I/Public/DataDefinitions/G2IOptionsParameters.h
@@ -4,6 +4,32 @@
 #include "Engine/DataAsset.h"
 #include "G2IOptionsParameters.generated.h"
 
+UENUM(BlueprintType)
+enum class EG2IScreenMode : uint8
+{
+	Windowed UMETA(DisplayName = "Windowed"),
+	WindowedFullscreen UMETA(DisplayName = "Windowed fullscreen"),
+	Fullscreen UMETA(DisplayName = "Fullscreen")
+};
+
+UENUM(BlueprintType)
+enum class EG2IScreenResolution : uint8
+{
+	Resolution1280On720 UMETA(DisplayName = "1280x720"),
+	Resolution1920On1080 UMETA(DisplayName = "1920x1080"),
+	Resolution2560On1440 UMETA(DisplayName = "2560x1440"),
+	Resolution3840On2160 UMETA(DisplayName = "3840x2160")
+};
+
+UENUM(BlueprintType)
+enum class EG2IFrameRate : uint8
+{
+	FPS30 UMETA(DisplayName = "30 FPS"),
+	FPS60 UMETA(DisplayName = "60 FPS"),
+	FPS120 UMETA(DisplayName = "120 FPS"),
+	FPSUnlimited UMETA(DisplayName = "Unlimited FPS")
+};
+
 /**
  * Options Defaults
  */
@@ -39,6 +65,36 @@ public:
 
 	// ==================== GRAPHICS ====================
 	// ==================== SCREEN ====================
+	EG2IScreenMode ScreenMode = EG2IScreenMode::WindowedFullscreen;
+	EG2IScreenResolution ScreenResolution = EG2IScreenResolution::Resolution1920On1080;
+	EG2IFrameRate FrameRate = EG2IFrameRate::FPSUnlimited;
+	bool bIsVSyncOn = true;
+	//float Brightness = 5.f;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Screen|View")
+	EG2IScreenMode DefaultScreenMode = EG2IScreenMode::WindowedFullscreen;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Screen|View")
+	EG2IScreenResolution DefaultScreenResolution = EG2IScreenResolution::Resolution1920On1080;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Screen|InGame")
+	EG2IFrameRate DefaultFrameRate = EG2IFrameRate::FPSUnlimited;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Screen|InGame")
+	bool bIsDefaultVSyncOn = true;
+	
+	/*UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Screen|InGame|Brightness", meta = (ClampMin = "0.0"))
+	float DefaultBrightness = 5.f;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Screen|InGame|Brightness", meta = (ClampMin = "0.0"))
+	float MinBrightness = 1.f;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Screen|InGame|Brightness", meta = (ClampMin = "0.0"))
+	float MaxBrightness = 10.f;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Screen|InGame|Brightness", meta = (ClampMin = "0.0"))
+	float StepBrightness = 1.f;*/
+	
 	// ==================== SOUNDS ====================
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Sound", meta = (ClampMin = "0.0", ClampMax = "10.0"))
 	float CommonVolume = 10.f;
@@ -52,8 +108,19 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Sound", meta = (ClampMin = "0.0", ClampMax = "10.0"))
 	float DialoguesVolume = 10.f;
 	
+private:
+	
+	UPROPERTY()
+	TObjectPtr<UGameUserSettings> Settings;
+	
 public:
 	
 	void SetupDefaultParameters();
+	
+	void SetScreenMode(EG2IScreenMode NewScreenMode);
+	void SetScreenResolution(EG2IScreenResolution NewScreenResolution);
+	void SetFrameRate(EG2IFrameRate NewFrameRate);
+	//void SetBrightness(float NewBrightness);
+	void SetVSync(bool bIsNewVSync);
 	
 };
